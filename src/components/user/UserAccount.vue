6f44ad92d7af402ae6211d355cd9241e8303a5a8
Oleg.L <testtesttest@test.ru>
2020-10-21 20:07:09 +0300

    code-review. Major changes in 404 and UserAccount.vue

--- src/components/user/UserAccount.vue
+++ src/components/user/UserAccount.vue
@@ -1,66 +1,80 @@
 <template>
     <v-container>
         <v-row>
-            <v-col class='col-12 col-sm-8 mx-auto'>
+            <v-col class="col-12 col-sm-8 mx-auto">
                 <v-card v-if="!loading">
+                    <!-- title -->
                     <v-card-title class="teal darken-2">
                         <span class="white--text">Account settings</span>
                         <v-spacer></v-spacer>
+                    </v-card-title>
 
-                        </v-card-title>
-                        <v-list>
-                        
+                    <v-list>
+                        <!-- name -->
                         <v-list-item>
                             <v-list-item-action>
-                            <v-icon>mdi-face</v-icon>
+                                <v-icon>mdi-face</v-icon>
                             </v-list-item-action>
 
                             <v-list-item-content>
-                            <v-list-item-title>{{ user.name }}</v-list-item-title>
+                                <v-list-item-title>
+                                    {{ user.name }}
+                                </v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
                         <v-divider inset></v-divider>
 
+                        <!-- email -->
                         <v-list-item>
                             <v-list-item-action>
-                            <v-icon>mdi-email</v-icon>
+                                <v-icon>mdi-email</v-icon>
                             </v-list-item-action>
 
                             <v-list-item-content>
-                            <v-list-item-title>{{ user.email }}</v-list-item-title>
+                                <v-list-item-title>
+                                    {{ user.email }}
+                                </v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
                         <v-divider inset></v-divider>
 
+                        <!-- password -->
                         <v-list-item>
                             <v-list-item-action>
-                            <v-icon>mdi-account-key-outline</v-icon>
+                                <v-icon>mdi-account-key-outline</v-icon>
                             </v-list-item-action>
 
                             <v-list-item-content>
-                            <v-list-item-title>●●●●●●●●</v-list-item-title>
+                                <v-list-item-title>●●●●●●●●</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
                         <v-divider inset></v-divider>
                     </v-list>
-                    
-                     <v-card-actions>
+
+                    <!-- actions -->
+                    <v-card-actions>
                         <v-spacer></v-spacer>
-                        <app-user-modal-edit :user="user" :loading="loading" :userName="userName"></app-user-modal-edit>
+                        <app-user-modal-edit
+                            :loading="loading"
+                            :user="user"
+                            :userName="userName"
+                        ></app-user-modal-edit>
                     </v-card-actions>
                 </v-card>
+
+                <!-- in loading progress -->
                 <div v-else>
                     <v-container>
                         <v-row>
                             <v-col class="col-xs-12 text-center pt-5">
                                 <v-progress-circular
-                                :size="50"
-                                :width="4"
-                                color="teal"
-                                indeterminate
+                                    :size="50"
+                                    :width="4"
+                                    color="teal"
+                                    indeterminate
                                 >
                                 </v-progress-circular>
                             </v-col>
@@ -73,26 +87,24 @@
 </template>
 
 <script>
-import UserModalEdit from './UserModalEdit'
+import UserModalEdit from "./UserModalEdit";
 
 export default {
-    computed: {
-        user () {
-            return this.$store.getters.user
-        },
-        loading () {
-            return this.$store.getters.loading
-        },
-        userName () {
-            return this.$store.state.user.user.name // forced check. For to first load of UserModalEdit (input 'editedName')
-        }
-    },
     components: {
         AppUserModalEdit: UserModalEdit,
     },
-}
+    computed: {
+        user() {
+            return this.$store.getters.user;
+        },
+        loading() {
+            return this.$store.getters.loading;
+        },
+        userName() {
+            return this.$store.state.user.user.name; // forced check user name. For to first load of UserModalEdit (input 'editedName')
+        },
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
